ed1c7797aa8389caa406eff0b988353278917673
tomek401273 <tomek371240@gmail.com>
2019-05-04 14:27:39 +0200

    cleaning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,3 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-//
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import {DetailsUploadComponent} from './upload/details-upload/details-upload.component';
-// import {FormUploadComponent} from './upload/form-upload/form-upload.component';
-// import {ListUploadComponent} from './upload/list-upload/list-upload.component';
-// import {UploadFileService} from './upload/upload-file.service';
-// import {HttpClientModule} from '@angular/common/http';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import {NgxFileHelpersModule} from 'ngx-file-helpers';
-// import {FileDropzoneDemoComponent} from './file-dropzone-demo/file-dropzone-demo.component';
-// import {FilePickerDemoComponent} from './file-picker-demo/file-picker-demo.component';
-// import {FormsModule} from '@angular/forms';
-// // import {MatCheckboxModule} from '@angular/material';
-// // import {MatButtonModule} from '@angular/material/typings/button';
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -23,8 +5,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-// import { FileHelpersModule } from 'ngx-file-helpers';
-
 import { AppComponent } from './app.component';
 import { ReadModePipe } from './read-mode.pipe';
 import { FilePickerDemoComponent } from './file-picker-demo/file-picker-demo.component';
@@ -35,28 +15,12 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {DetailsUploadComponent} from './upload/details-upload/details-upload.component';
 import {FormUploadComponent} from './upload/form-upload/form-upload.component';
-import {ListUploadComponent} from './upload/list-upload/list-upload.component';
 
 @NgModule({
-  // declarations: [
-  //   AppComponent,
-  //   DetailsUploadComponent,
-  //   FormUploadComponent,
-  //   ListUploadComponent
-  // ],
-  // imports: [
-  //   BrowserModule,
-  //   AppRoutingModule,
-  //   HttpClientModule,
-  //   BrowserAnimationsModule,
-  //   NgxFileHelpersModule,
-  //
-  // ],
   declarations: [
     AppComponent,
     DetailsUploadComponent,
     FormUploadComponent,
-    ListUploadComponent,
     ReadModePipe,
     FilePickerDemoComponent,
     FileDropzoneDemoComponent
