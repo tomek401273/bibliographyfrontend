65756550c7bcdd7f638f34ce53036f9881f7f650
tomek401273 <tomek371240@gmail.com>
2019-05-04 14:03:44 +0200

    dropzone-works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+//
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import {DetailsUploadComponent} from './upload/details-upload/details-upload.component';
+// import {FormUploadComponent} from './upload/form-upload/form-upload.component';
+// import {ListUploadComponent} from './upload/list-upload/list-upload.component';
+// import {UploadFileService} from './upload/upload-file.service';
+// import {HttpClientModule} from '@angular/common/http';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import {NgxFileHelpersModule} from 'ngx-file-helpers';
+// import {FileDropzoneDemoComponent} from './file-dropzone-demo/file-dropzone-demo.component';
+// import {FilePickerDemoComponent} from './file-picker-demo/file-picker-demo.component';
+// import {FormsModule} from '@angular/forms';
+// // import {MatCheckboxModule} from '@angular/material';
+// // import {MatButtonModule} from '@angular/material/typings/button';
 
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+// import { FileHelpersModule } from 'ngx-file-helpers';
+
 import { AppComponent } from './app.component';
-import {DetailsUploadComponent} from './upload/details-upload/details-upload.component';
-import {FormUploadComponent} from './upload/form-upload/form-upload.component';
-import {ListUploadComponent} from './upload/list-upload/list-upload.component';
-import {UploadFileService} from './upload/upload-file.service';
-import {HttpClientModule} from '@angular/common/http';
+import { ReadModePipe } from './read-mode.pipe';
+import { FilePickerDemoComponent } from './file-picker-demo/file-picker-demo.component';
+import { FileDropzoneDemoComponent } from './file-dropzone-demo/file-dropzone-demo.component';
+import {FileHelpersModule} from 'ngx-file-helpers';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DetailsUploadComponent,
-    FormUploadComponent,
-    ListUploadComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [UploadFileService],
+  // declarations: [
+  //   AppComponent,
+  //   DetailsUploadComponent,
+  //   FormUploadComponent,
+  //   ListUploadComponent
+  // ],
+  // imports: [
+  //   BrowserModule,
+  //   AppRoutingModule,
+  //   HttpClientModule,
+  //   BrowserAnimationsModule,
+  //   NgxFileHelpersModule,
+  //
+  // ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatButtonModule, MatTabsModule, MatToolbarModule, FileHelpersModule ],
+  declarations: [ AppComponent, ReadModePipe, FilePickerDemoComponent, FileDropzoneDemoComponent ],
+  // providers: [UploadFileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/file-dropzone-demo/file-dropzone-demo.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { ReadFile, ReadMode } from 'ngx-file-helpers';
+
+@Component({
+  selector: 'app-file-dropzone-demo',
+  templateUrl: './file-dropzone-demo.component.html',
+  styleUrls: ['./file-dropzone-demo.component.css']
+})
+export class FileDropzoneDemoComponent {
+  public readMode = ReadMode.dataURL;
+  public isHover: boolean;
+  public files: Array<ReadFile> = [];
+  
+  addFile(file: ReadFile) {
+    console.log('file file');
+    console.log(file.name);
+    this.files.push(file);
+  }
+}
